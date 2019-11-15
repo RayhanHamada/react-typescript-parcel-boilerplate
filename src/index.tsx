372d873f375388816378eacf5fa817028a4408eb
RayhanHamada <hamadadesk1999@gmail.com>
2019-11-15 19:56:53 +0700

    initialize repo

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+
+import "./styles.css";
+import "./styles.scss";
+
+ReactDOM.render( /*react component here*/ , document.getElementById("app"));
