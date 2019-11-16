f643421743af82393515f6830ee5c75346ee2cfb
RayhanHamada <hamadadesk1999@gmail.com>
2019-11-16 12:53:46 +0700

    commit lagi

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 
 // import reducer dibawah
-import counterReducer from './counterReducer'
+import counterReducer from "./counterReducer";
 
 export default combineReducers({
-    counterReducer
+  counterReducer
 });
--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,6 @@ import { createStore } from "redux";
 import rootReducer from "./reducer";
 
 export default createStore(
-    rootReducer,
-    {} // ganti pake initial state (kalau mau) 
+  rootReducer,
+  {} // ganti pake initial state (kalau mau)
 );
