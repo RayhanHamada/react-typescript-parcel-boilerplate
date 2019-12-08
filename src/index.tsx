eb513a409bd0e76f1970d10af1f5126bc368cf07
RayhanHamada <hamadadesk1999@gmail.com>
2019-12-08 23:07:41 +0700

    another commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
-import "./styles.css";
 import "./styles.scss";
 
 ReactDOM.render( /*react component here*/ , document.getElementById("app"));
